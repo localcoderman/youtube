4ca596fb7d7ba20e118e33887cfe5f3b9b24d90b
Abdullah Shahbaz <mohammadabdullah5341@gmail.com>
2026-02-19 12:41:10 +0500

    feat: add getUserWatchHistory function and update users routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { accessRefreshToken, registerUser } from "../controllers/user.controller.js";
+import { accessRefreshToken, changeCurrentPassword, getCurrentUser, getUserChannelProfile, getUserWatchHistory, registerUser, updateAccountDetail, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer_middleware.js";
 import { loginUser, logoutUser } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -20,6 +20,14 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refreshtoken").post(accessRefreshToken)
-
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-details").patch(verifyJWT, updateAccountDetail)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/coverImage").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+/* params se data fetch ho raha hai es liyay colon ka khiyal rkhna 
+or us k baad wohi naam rkho jis se data ly rhy ho controllers mai */
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+router.route("/user-wacthHistory").get(verifyJWT, getUserWatchHistory)
 
 export default router;
